4e508b3d5e553d260aa7d3eaacd68c5c43bba912
John Capper <capper@mines.edu>
2020-04-30 23:41:40 -0600

    Wrote README.md and updated .gitignore

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # mannkendalltest
-Python functions for Mann Kendall and Seasonal Kendall Tests
+Simple Python functions for Mann Kendall and Seasonal Kendall Tests
+## **Dependencies**
+You must have the following installed:
+- [numpy](https://github.com/numpy/numpy)
+- [scipy](https://github.com/scipy/scipy)
+
+## **Functions**
+All functions return a named tuple containing:
+- *s*: the S statistic
+- *var_s*: the variance of S
+- *z*: the Z-score associated with S statistic. 
+- *p*: the probability value. Area under normal curve
+
+**Mann Kendall Test**  
+*mk_test(vals)*
+- Parameters:
+    - *vals*: set of data to analyze. Must be in chronological order
+
+
+**Seasonal Kendall Test**  
+*sk_test(vals)*
+- Parameters:
+    - *vals*: set of data to analyze. Must be in chronological order
+    - *period*: number of seasons contained in data. Default is 12 (Eg 12 months in a year)
--- mann_kendall.py
+++ mann_kendall.py
@@ -1,19 +1,21 @@
+__author__ = 'John Capper'
+
 import scipy.stats as st
 import numpy as np
 from collections import namedtuple
 from math import sqrt
 
 ## Seasonal Kendall Test ##
-def sk_test(vals, alpha=0.05, period=12):
-    if isinstance(vals, list):
+def sk_test(vals, period=12):
+    if not isinstance(vals, np.ndarray):
         vals = np.asarray(vals)
 
     s_prime = 0
     total_variance = 0
     for season in range(period):
-        s, variance = mk_test(vals[season::period], alpha=alpha, get_z=False)
-        s_prime += s
-        total_variance += variance
+        test_result = mk_test(vals[season::period])
+        s_prime += test_result.s
+        total_variance += test_result.var_s
 
     z = calc_z(s_prime, total_variance)
     p = st.norm.cdf(z)
@@ -22,19 +24,18 @@ def sk_test(vals, alpha=0.05, period=12):
     return Mann_Kendall(s_prime, total_variance, z, 1-p)
 
 ## Mann-Kendall Test ##
-def mk_test(vals, alpha=0.05, get_z=True):
+def mk_test(vals):
+    if not isinstance(vals, np.ndarray):
+        vals = np.asarray(vals)
+
     s, ties = calc_s(vals)
     variance = var_s(len(vals), ties)
 
-    if not get_z:
-        return s, variance
-
     z = calc_z(s, variance)
     p = st.norm.cdf(z) # Obtain area under normal curve
-    conclusion = interpret_z(z, p, alpha)
 
-    Mann_Kendall = namedtuple("Mann_Kendall", ['s', 'var_s', 'z', 'p', 'conclusion'])
-    return Mann_Kendall(s, variance, z, 1-p, conclusion)
+    Mann_Kendall = namedtuple("Mann_Kendall", ['s', 'var_s', 'z', 'p'])
+    return Mann_Kendall(s, variance, z, 1-p)
 
 ## Calculate S Statistic ##
 def calc_s(vals):
@@ -43,7 +44,7 @@ def calc_s(vals):
     ties = {}
     for k in range(n-1):
         for j in range(k+1, n):
-            sign = sgn(vals[j] - vals[k])
+            sign = _sgn(vals[j] - vals[k])
             if sign == 0:
                 ties[vals[k]] = ties.get(vals[k], 0) + 1
             s += sign
@@ -65,20 +66,10 @@ def calc_z(s, variance):
     if not s:
         return 0
 
-    return (s-sgn(s))/sqrt(variance)
-
-## Form a conclusion ##
-def interpret_z(z, p, alpha):
-    conclusion = "No"
-    if p > 1-alpha:
-        if z > 0:
-            conclusion = 'an increasing'
-        else:
-            conclusion = 'a decreasing'
-    return "There is %s Trend" % conclusion
+    return (s - _sgn(s)) / sqrt(variance)
 
 ## Sign function ##
-def sgn(n):
+def _sgn(n):
     if n == 0:
         return 0
     return n/abs(n)
